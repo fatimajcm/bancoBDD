4bc44fe1daac05785f263bb6e92a831c19382a3c
fatimajcm <fatimamacias500@outlook.com>
2023-06-19 02:08:40 -0600

    admin view

--- js/script.js
+++ js/script.js
@@ -9,11 +9,30 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
   
     // Realiza la validación del usuario y la contraseña
     if (username === "admin" && password === "admin123") {
-      alert("Inicio de sesión exitoso");
-      // Aquí puedes redirigir al usuario a la página principal    
+      alert("Inicio de sesión exitoso");  
       window.location.href = "../html/viewAdmin.html";
     } else {
       alert("Credenciales inválidas");
     }
   });
+
+function addClient(){
+  var container = document.querySelector('.container');
+
+    // Crea un elemento div
+    var newDiv = document.createElement('div');
+
+    // Agrega código HTML al nuevo div
+    // CAMBIAR POR UN FORM QUE ENVIE DATOS DE NOMBRE, APELLIDO Y CORREO
+    newDiv.innerHTML = `<p>ID Cliente:</p>
+    <p>Nombre: </p>
+    <p>Apellido: </p>
+    <p>Dirección: </p>
+    <p>Teléfono: </p>
+    <p>Correo Electrónico: </p>
+  `;
+
+    // Agrega el nuevo div al contenedor
+    container.appendChild(newDiv);
+}
   
